b00932ff0d0c1d24526b1f7a18d9ccdc99f16ae5
dwincahya <dwincahya8@gmail.com>
2025-03-29 18:47:03 +0700

    Add pagination

--- src/controller/DiseaseController.js
+++ src/controller/DiseaseController.js
@@ -1,8 +1,8 @@
-import prisma from '../utils/prismaClient.js';
+import models from '../model/models.js';
 
 const getAllDiseases = async (request, h) => {
   try {
-    const diseases = await prisma.disease.findMany({
+    const diseases = await models.Disease.findMany({
       include: {
         symptoms: {
           select: {
@@ -19,13 +19,16 @@ const getAllDiseases = async (request, h) => {
     });
 
     const formattedDiseases = diseases.map((disease) => ({
-      ...disease,
+      id: disease.id,
+      name: disease.name,
+      image: disease.image,
+      description: disease.description,
       symptoms: disease.symptoms.map((s) => s.symptom)
     }));
 
     return h.response(formattedDiseases).code(200);
   } catch (error) {
-    console.error('Error fetching diseases:', error.message);
+    console.error('Error fetching all diseases:', error.message);
     return h.response({ error: 'Failed to fetch diseases' }).code(500);
   }
 };
@@ -34,7 +37,7 @@ const getDiseaseById = async (request, h) => {
   const { id } = request.params;
 
   try {
-    const disease = await prisma.disease.findUnique({
+    const disease = await models.disease.findUnique({
       where: { id },
       include: {
         symptoms: {
@@ -72,7 +75,7 @@ const getDiseasesBySymptoms = async (request, h) => {
 
   try {
     const ids = symptomIds.split(',');
-    const diseases = await prisma.disease.findMany({
+    const diseases = await models.disease.findMany({
       where: {
         symptoms: {
           some: {
@@ -111,11 +114,63 @@ const getDiseasesBySymptoms = async (request, h) => {
   }
 };
 
+const getDiseasesWithPagination = async (request, h) => {
+  const { page = 1, limit = 10 } = request.query;
+
+  try {
+    const skip = (page - 1) * limit;
+    const diseases = await models.Disease.findMany({
+      skip,
+      take: parseInt(limit),
+      include: {
+        symptoms: {
+          select: {
+            symptom: {
+              select: {
+                id: true,
+                name: true,
+                description: true
+              }
+            }
+          }
+        }
+      }
+    });
+
+    const totalDiseases = await models.Disease.count();
+    const totalPages = Math.ceil(totalDiseases / limit);
+    const formattedDiseases = diseases.map((disease) => ({
+      id: disease.id,
+      name: disease.name,
+      image: disease.image,
+      description: disease.description,
+      symptoms: disease.symptoms.map((s) => s.symptom)
+    }));
+
+    return h
+      .response({
+        data: formattedDiseases,
+        pagination: {
+          totalItems: totalDiseases,
+          totalPages,
+          currentPage: parseInt(page),
+          itemsPerPage: parseInt(limit)
+        }
+      })
+      .code(200);
+  } catch (error) {
+    console.error('Error fetching diseases with pagination:', error.message);
+    return h
+      .response({ error: 'Failed to fetch diseases with pagination' })
+      .code(500);
+  }
+};
+
 const updateDisease = async (request, h) => {
   const { id } = request.params;
   const { image, description } = request.payload;
   try {
-    const updatedDisease = await prisma.disease.update({
+    const updatedDisease = await models.disease.update({
       where: { id },
       data: { image, description }
     });
@@ -130,4 +185,10 @@ const updateDisease = async (request, h) => {
   }
 };
 
-export { getAllDiseases, updateDisease, getDiseaseById, getDiseasesBySymptoms };
+export {
+  getAllDiseases,
+  updateDisease,
+  getDiseaseById,
+  getDiseasesBySymptoms,
+  getDiseasesWithPagination
+};
