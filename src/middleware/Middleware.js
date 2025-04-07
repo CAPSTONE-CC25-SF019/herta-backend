d7630de6ad2e09587574c122f1cf03bd369a2d80
tirtahakimpambudhi <awebthp@gmail.com>
2025-04-07 08:19:48 +0700

    feat(middleware): :sparkles: Update middleware in the file Middleware.js

--- src/middleware/Middleware.js
+++ src/middleware/Middleware.js
@@ -17,7 +17,6 @@ export default class Middleware {
   constructor() {
     this.name = '';
     this.version = '1.0.0';
-    this.protectedRoutes = [];
   }
 
   /**
@@ -28,32 +27,5 @@ export default class Middleware {
   // eslint-disable-next-line no-unused-vars
   register(server, options) {
     throw new Error('Method register() must be implemented by subclass');
-  }
-
-  /**
-   * Function to handle request
-   * @param {Hapi.Request} request - Hapi Request object
-   * @param {Hapi.h} h - Hapi response toolkit
-   * @returns {Hapi.Response} Response object
-   */
-  // eslint-disable-next-line no-unused-vars
-  async handler(request, h) {
-    throw new Error('Method handler() must be implemented by subclass');
-  }
-
-  /**
-   * Check if a route requires
-   * @param {string} path - The route path to check
-   * @returns {boolean} - Whether the route requires authentication
-   */
-  routeRequires(path) {
-    return this.protectedRoutes.some((route) => {
-      // Handle wildcard routes (e.g., '/api/*')
-      if (route.endsWith('*')) {
-        const prefix = route.slice(0, -1);
-        return path.startsWith(prefix);
-      }
-      return path === route;
-    });
   }
 }
