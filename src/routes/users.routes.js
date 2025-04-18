7e05e6adeeaf982062e130301bbf97df1f2a76dd
tirtahakimpambudhi <awebthp@gmail.com>
2025-04-18 19:09:51 +0700

    feat(routes): :sparkles: Update routes in the file users.routes.js

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,60 +1,63 @@
-import Boom from '@hapi/boom';
-
 import Hash from '../config/hash/bcrypt.js';
 import jwe from '../config/jwk/jwe.js';
 import jws from '../config/jwk/jws.js';
 import Jwe from '../config/jwt/jwe.js';
 import Jws from '../config/jwt/jws.js';
 import UsersController from '../controller/UsersController.js';
+import AuthJwtMiddleware from '../middleware/AuthJwtMiddleware.js';
 import UsersService from '../service/UsersService.js';
 import UsersValidation from '../validation/users.validation.js';
 
-import AuthJwtMiddleware from '../middleware/AuthJwtMiddleware.js';
-
 export default async function (server) {
-  // Initialize dependencies
-  const usersService = new UsersService(Hash, {
-    jwe: new Jwe({ ...jwe, algorithm: 'ECDH-ES+A256KW' }),
-    jws: {
-      accessToken: new Jws({
-        privateKey: jws.accessTokenPrivateKey,
-        publicKey: jws.accessTokenPublicKey,
-        algorithms: ['EdDSA']
-      }),
-      refreshToken: new Jws({
-        privateKey: jws.refreshTokenPrivateKey,
-        publicKey: jws.refreshTokenPublicKey,
-        algorithms: ['EdDSA']
-      })
-    }
-  });
-  const usersController = new UsersController(usersService, UsersValidation);
-
   // Create JWE and JWS instances
   const jweInstance = new Jwe({ ...jwe, algorithm: 'ECDH-ES+A256KW' });
   const jwsAccessToken = new Jws({
     privateKey: jws.accessTokenPrivateKey,
     publicKey: jws.accessTokenPublicKey,
-    algorithms: ['EdDSA']
+    algorithm: 'EdDSA'
   });
+  // Initialize dependencies
+  const usersService = new UsersService(new Hash(), {
+    jwe: jweInstance,
+    jws: {
+      accessToken: jwsAccessToken,
+      refreshToken: new Jws({
+        privateKey: jws.refreshTokenPrivateKey,
+        publicKey: jws.refreshTokenPublicKey,
+        algorithm: 'EdDSA'
+      })
+    }
+  });
+
+  const usersController = new UsersController(usersService, UsersValidation);
 
   // Create AuthJwtMiddleware instance
   const authMiddleware = new AuthJwtMiddleware({
+    payloadTokenSchema: UsersValidation.payloadToken,
     name: process.env.JWT_AUTH_NAME,
     jwe: jweInstance, // Pass JWE instance
     jws: jwsAccessToken, // Pass JWS instance
     protectedRoutes: [
-      { path: '/api/v1/users/update', method: 'PUT', role: 'user' },
-      { path: '/api/v1/users/delete', method: 'DELETE', role: 'admin' }
+      { path: '/api/v1/users', method: 'PUT' },
+      { path: /^\/api\/v1\/users\/.*$/, method: 'DELETE', role: 'admin' },
+      { path: /^\/api\/v1\/users(\?.*)?$/, method: 'GET', role: 'admin' }, // list users
+      { path: '/api/v1/users/profile', method: 'GET' } // self profile
     ],
-    roleHierarchy: { admin: ['editor', 'user'], editor: ['user'] }
+    roleHierarchy: { admin: ['user'] }
   });
 
   // Register the middleware
   await authMiddleware.register(server);
 
-
   server.route([
+    {
+      method: 'GET',
+      path: '/api/v1/users',
+      options: {
+        auth: 'jwt',
+        handler: (request, h) => usersController.getAllUsers(request, h)
+      }
+    },
     {
       method: 'POST',
       path: '/api/v1/users/register',
@@ -70,8 +73,15 @@ export default async function (server) {
       }
     },
     {
+      method: 'GET',
+      path: '/api/v1/users/refresh-token',
+      options: {
+        handler: (request, h) => usersController.refreshToken(request, h)
+      }
+    },
+    {
       method: 'PUT',
-      path: '/api/v1/users/update',
+      path: '/api/v1/users',
       options: {
         auth: 'jwt',
         handler: (request, h) => usersController.update(request, h)
@@ -79,20 +89,9 @@ export default async function (server) {
     },
     {
       method: 'DELETE',
-      path: '/api/v1/users/delete',
+      path: '/api/v1/users/{email}',
       options: {
         auth: 'jwt',
-        pre: [
-          {
-            method: (request) => {
-              // RBAC Check - Only admin can delete users
-              if (!request.hasRoleAccess('ADMIN')) {
-                throw Boom.forbidden('Only admin can delete users');
-              }
-              return true;
-            }
-          }
-        ],
         handler: (request, h) => usersController.delete(request, h)
       }
     }
