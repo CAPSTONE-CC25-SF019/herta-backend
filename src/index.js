593bc789c7515f48a521f9a0ba3daca0bfea8b53
tirtahakimpambudhi <awebthp@gmail.com>
2025-04-24 20:28:14 +0700

    feat(main): :sparkles: Update feature in file index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import Hapi from '@hapi/hapi';
+import Inert from '@hapi/inert';
+import Vision from '@hapi/vision';
+import HapiSwagger from 'hapi-swagger';
 import Joi from 'joi';
 import process from 'node:process';
-
 import Routes from './routes/routes.js';
-import UsersRoutes from './routes/users.routes.js';
-
+import statusToCode from './utils/statusToCode.js';
 /**
  * Function init for initialize api
  * @param {String} host  the host location api
@@ -18,15 +19,61 @@ const init = async (host, port) => {
       port,
       routes: {
         cors: {
-          origin: ['*']
+          origin: ['*'],
+          credentials: true
         }
       }
     });
 
-    await UsersRoutes(server);
-    server.route(Routes);
+    const swaggerOptions = {
+      info: {
+        title: 'Herta API Documentation',
+        version: process.env.API_VERSION,
+      },
+      securityDefinitions: {
+        Bearer: {
+          type: 'apiKey',
+          name: 'Authorization',
+          in: 'header'
+        }
+      },
+    };
 
+    await server.register([
+      Inert,
+      Vision,
+      {
+        plugin: HapiSwagger,
+        options: swaggerOptions,
+        grouping: 'tags'
+      }
+    ]);
+    await Routes(server);
     server.validator(Joi);
+    server.ext('onPreResponse', (request, h) => {
+      const response = request.response;
+
+      if (response.isBoom) {
+        const { output, message, data } = response;
+        const statusCode = output.statusCode;
+
+        const customError = {
+          errors: [
+            {
+              title: statusToCode(statusCode),
+              details: data || message,
+              status: statusCode,
+              code: statusToCode(statusCode)
+            }
+          ]
+        };
+
+        return h.response(customError).code(statusCode);
+      }
+
+      return h.continue;
+    });
+
     await server.start();
     console.log(`Server running on ${server.info.uri}`);
   } catch (error) {
