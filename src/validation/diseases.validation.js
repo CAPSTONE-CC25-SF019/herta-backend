59fd9c9c840d058d5ede580a505191414e216ae6
tirtahakimpambudhi <awebthp@gmail.com>
2025-04-26 20:33:21 +0700

    feat(validation): :sparkles: Update validation in the file diseases.validation.js

--- src/validation/diseases.validation.js
+++ src/validation/diseases.validation.js
@@ -1,19 +1,30 @@
 import Joi from 'joi';
+
 import UsersValidation from './users.validation.js';
 
 export default {
   pagination: UsersValidation.pagination,
 
   update: Joi.object({
-    image: Joi.string().uri().min(5).required().description('Image URL representing the disease').messages({
-      'string.uri': 'invalid format image path',
-      'string.min': 'the image path must be greater than five character',
-      'any.required': 'this image is required'
-    }),
-    description: Joi.string().min(1).required().description('Updated description of the disease').messages({
-      'string.min': 'the description disease must be greater than one character',
-      'any.required': 'this description is required'
-    })
+    image: Joi.string()
+      .uri()
+      .min(5)
+      .required()
+      .description('Image URL representing the disease')
+      .messages({
+        'string.uri': 'invalid format image path',
+        'string.min': 'the image path must be greater than five character',
+        'any.required': 'this image is required'
+      }),
+    description: Joi.string()
+      .min(1)
+      .required()
+      .description('Updated description of the disease')
+      .messages({
+        'string.min':
+          'the description disease must be greater than one character',
+        'any.required': 'this description is required'
+      })
   }).label('DiseaseUpdateBody'),
 
   filterByNames: Joi.object({
@@ -26,7 +37,9 @@ export default {
         }
         return value;
       }, 'Convert string to array')
-      .description('Filter diseases by name(s), accepting either a single name or an array of names')
+      .description(
+        'Filter diseases by name(s), accepting either a single name or an array of names'
+      )
       .messages({
         'alternatives.types': 'name must be a string or array of strings',
         'array.base': 'name must be an array of strings'
@@ -34,9 +47,13 @@ export default {
   }).label('FilterByNamesQuery'),
 
   filterById: Joi.object({
-    id: Joi.string().uuid().required().description('The unique UUID identifier of the disease').messages({
-      'string.guid': 'id must be a valid UUID',
-      'any.required': 'this id is required'
-    })
-  }).label('FilterByIDQuery'),
+    id: Joi.string()
+      .uuid()
+      .required()
+      .description('The unique UUID identifier of the disease')
+      .messages({
+        'string.guid': 'id must be a valid UUID',
+        'any.required': 'this id is required'
+      })
+  }).label('FilterByIDQuery')
 };
