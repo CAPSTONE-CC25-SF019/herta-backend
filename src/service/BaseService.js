a92082297888e1cee7c97c31748bc1d377ea745c
tirtahakimpambudhi <awebthp@gmail.com>
2025-05-06 20:28:20 +0700

    feat(service): :sparkles: Update service in the file BaseService.js

--- src/service/BaseService.js
+++ src/service/BaseService.js
@@ -144,7 +144,7 @@ export default class BaseService {
           fieldName: null,
           fieldValue: null
         });
-        errService.message = `the ${error.meta.modelName.toLowerCase()} ${error.meta.cause.toString().includes("found none") ? "not found" :error.meta.cause}`;
+        errService.message = `the ${error.meta.modelName.toLowerCase()} ${error.meta.cause.toString().includes('found none') ? 'not found' : error.meta.cause}`;
         throw errService;
       }
     }
